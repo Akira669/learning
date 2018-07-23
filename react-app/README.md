cea21bb8bfcbd2885307dd673941ca8bd6479f7c
Family <family@MacBook-Pro-de-Family.local>
2018-07-23 00:32:24 -0500

    feature(react)

---
+++ react-app/README.md
@@ -0,0 +1,13 @@
+# repositorio de practicas y ejemplos de tecnologias (WEB)
+
+### CSS 
+> Hojas de estilo
+
+### WEBPACK
+> empaquetador de modulos ó module bundler
+
+### REACT
+> Libreria de JS - ES6
+> virtual DOM
+> JSX
+
---
+++ react-app/src/components/global/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p> tel: 5575646653 </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
